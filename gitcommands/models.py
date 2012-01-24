198c2c8ab28a95cb68de4a074e012219698f3651
Venkata Ramana C <arjunchitturi@gmail.com>
2012-01-25 00:32:17 +0530

    added new model attributes

--- gitcommands/models.py
+++ gitcommands/models.py
@@ -13,19 +13,33 @@ class GitUser(models.Model):
 
 class GitRepository(models.Model):
     repo_user = models.ForeignKey(GitUser, related_name='repo_user')
+    repo_owner = models.CharField(max_length=300)
     repo_name = models.CharField(max_length=300)
+    repo_description = models.CharField(max_length=300, null=True)
+    repo_language = models.CharField(max_length=100, null=True)
+    repo_created = models.DateTimeField(null=True)
+    repo_pushed = models.DateTimeField(null=True)
+    repo_watchers = models.PositiveIntegerField(null=True)
+    repo_private = models.BooleanField(default=False)
     repo_url = models.URLField()
 
 
 class GitCommit(models.Model):
-
+    '''
     MODE_CHOICES = (
         ('A', 'Added'),
         ('R', 'Removed'),
         ('M', 'Modified'),
     )
+    '''
     commit_user = models.ForeignKey(GitUser, related_name='committer')
     commit_message = models.CharField(max_length=300)
     commit_time = models.DateTimeField(null=True)
-    commit_mode = models.CharField(max_length=1, choices=MODE_CHOICES)
+    #commit_add = models.CharField(max_length=300)
+    commit_mode = models.CharField(max_length=10)
+    #commit_modify = models.CharField(max_length=300)
+    commit_committer = models.CharField(max_length=300)
+    commit_author = models.CharField(max_length=300)
+    commit_parents = models.CharField(max_length=300)
+    commit_tree = models.CharField(max_length=300)
     commit_url = models.URLField()
